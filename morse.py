26b6a69b764a87f3ade7d5d0496432d1e64aa2c8
Karlis Goba <karlis.goba@gmail.com>
2017-06-02 17:22:55 +0200

    Wave reading

--- morse.py
+++ morse.py
@@ -2,6 +2,7 @@
 
 import sys
 import math
+import random
 import numpy as np
 
 morseTable = {
@@ -34,7 +35,17 @@ morseTable = {
     'Z':    '--..',
     'Q':    '--.-',
 #   ' ':    '---.',
-#   ' ':    '----'    
+#   ' ':    '----',
+    '0':    '-----',
+    '1':    '.----',
+    '2':    '..---',
+    '3':    '...--',
+    '4':    '....-',
+    '5':    '.....',
+    '6':    '-....',
+    '7':    '--...',
+    '8':    '---..',
+    '9':    '----.',    
 }
 
 def stringToKey(str):
@@ -53,7 +64,7 @@ def stringToKey(str):
                 key += '`'
             key += morseTable[uc]
         lastChar = thisChar
-    return key
+    return key + '`'
 
 def keyToTimings(key, wpm = 25):
     s = wpm
@@ -99,19 +110,93 @@ def timingsToEnvelope(timings, fs = 200):
             if t > 0:
                 env.append(1)
             else:
-                env.append(0)
+                env.append(-1)
                 
     return env
 
-string = 'ALL YOUR BASE ARE BELONG US'
-key = stringToKey(string)
-timings = keyToTimings(key, 20)
-env = timingsToEnvelope(timings, 50)
+def generateRandomString():
+    chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    words = []
+    for idxWord in range(10):
+        length = int(0.5 + random.gauss(4.5, 1.5))
+        word = ''.join([random.choice(chars) for x in range(length)])
+        words.append(word)
+    return ' '.join(words)
 
-print string
-print key
-print timings
-print env
+def generateTrainingSample(wpm = 20, fs = 50):
+    string = generateRandomString()
+    key = stringToKey(string)
+    timings = keyToTimings(key, wpm)
+    env = timingsToEnvelope(timings, fs)
+    
+    return (string, env)
+
+def generateCorrelators(wpm = 20, fs = 50):
+    cor = dict()
+    for c in morseTable:
+        key = stringToKey('`' + c)
+        timings = keyToTimings(key, wpm)
+        env = timingsToEnvelope(timings, fs)
+        cor[c] = np.array(env)
+    return cor
+
+def example1():
+    string = 'ALL YOUR BASE ARE BELONG US'
+    key = stringToKey(string)
+    timings = keyToTimings(key, 20)
+    env = timingsToEnvelope(timings, 50)
+
+    print string
+    print key
+    print timings
+    print env
+
+def extractEnv(sig, fsOrig, fs):
+    #bandpass = spsig.firwin(50, [f1, f2], pass_zero=False)
+    env = np.abs(sig)
+    lowpass = spsig.firwin(50, 100.0, nyq = fsOrig / 2.0)
+    env = spsig.lfilter(lowpass, 1, env)
+
+    for N in range(1, 50):
+        M = fsOrig * N / float(fs)
+        M = int(0.5 + M)
+        fs2 = float(fsOrig * N) / M
+        relErr = math.fabs(fs2/fs - 1.0)
+        if relErr < 1E-3:
+            print N, "/", M
+            break
+
+    env = spsig.resample_poly(env, N, M)
+    env -= np.mean(env)
+    env /= np.max(env)
+    
+    return env
+
+import wave
+import struct
+import scipy.signal as spsig
+
+wav = wave.open(sys.argv[1])
+fs = wav.getframerate()
+assert(wav.getnchannels() == 1)
+assert(wav.getsampwidth() == 2)
+count = wav.getnframes()
+sig = struct.unpack('<' + 'h' * count, wav.readframes(count))
+sig = np.array(sig) / 32768.0
+
+fs2 = 100
+wpm = 26
+
+env = extractEnv(sig, fs, fs2)
+corr = generateCorrelators(wpm = wpm, fs = fs2)
+
+c1 = spsig.convolve(env, corr['C'], mode = 'same')
+c2 = spsig.convolve(env, corr['R'], mode = 'same')
+
+print c1[200:280]
+print c2[200:280]
+
+sys.exit(0)
 
 def appendChain(states, arcs, newStates, prevState = -1):
     for (emit, label) in newStates:
@@ -123,37 +208,38 @@ def appendChain(states, arcs, newStates, prevState = -1):
 
     return idxState
 
-states = list()
-arcs = list()
+def buildHMM():
+    states = list()
+    arcs = list()
 
-stateChar = appendChain(states, arcs, ((-1, None),) * 3)
-stateWord = appendChain(states, arcs, ((-1, None),) * 4, stateChar)
+    stateChar = appendChain(states, arcs, ((-1, None),) * 3)
+    stateWord = appendChain(states, arcs, ((-1, None),) * 4, stateChar)
 
-stateBreak1 = appendChain(states, arcs, ((-1, None),) * 1, stateWord)
-stateBreak2 = appendChain(states, arcs, ((-1, None),) * 3, stateBreak1)
-arcs.append((stateBreak2, stateBreak1, 1.000))
-stateBreak = appendChain(states, arcs, ((-1, None),) * 1, stateBreak2)
+    stateBreak1 = appendChain(states, arcs, ((-1, None),) * 1, stateWord)
+    stateBreak2 = appendChain(states, arcs, ((-1, None),) * 3, stateBreak1)
+    arcs.append((stateBreak2, stateBreak1, 1.000))
+    stateBreak = appendChain(states, arcs, ((-1, None),) * 1, stateBreak2)
 
-for c in morseTable:
-    st = []
-    for x in morseTable[c]:       
-        if x == '.':
-            st.append([-1, None])
-        elif x == '-':
-            st.append([1, None])
-    st[-1][1] = c
+    for c in morseTable:
+        st = []
+        for x in morseTable[c]:       
+            if x == '.':
+                st.append([-1, None])
+            elif x == '-':
+                st.append([1, None])
+        st[-1][1] = c
 
-    stateBegin = len(states)
-    stateEnd = appendChain(states, arcs, st)
-    arcs.append((stateChar, stateBegin, 1.000))
-    arcs.append((stateWord, stateBegin, 1.000))
-    arcs.append((stateBreak, stateBegin, 1.000))
-    arcs.append((stateEnd, 0, 1.000))
-         
-print arcs
-print states
-    
-pTrans = dict()
+        stateBegin = len(states)
+        stateEnd = appendChain(states, arcs, st)
+        arcs.append((stateChar, stateBegin, 1.000))
+        arcs.append((stateWord, stateBegin, 1.000))
+        arcs.append((stateBreak, stateBegin, 1.000))
+        arcs.append((stateEnd, 0, 1.000))
+             
+    print arcs
+    print states
+        
+    pTrans = dict()
 
 #keras.layers.convolutional.Conv1D(filters, kernel_size, padding='causal', use_bias=True)
 #keras.layers.recurrent.GRU(numHidden)   # or LSTM
